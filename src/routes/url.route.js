0ed1ceb7d01aea452a944e6a2a2abeb06cb2d0f4
ankitsikeriya <ankitsiker0@gmail.com>
2025-09-15 18:44:37 +0530

    backend

--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createUrl,createCustomUrl} from "../controllers/url.controller.js";
-const router = Router();
+const router = Router(); 
 
 router.post("/create", createUrl);
 //custom short url route should be at the end of all routes
